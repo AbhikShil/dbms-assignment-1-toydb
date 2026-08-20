405a7b7353226f0537ae389f8a2b9dec20f42ae6
AbhikShil <abhikshil@gmail.com>
2026-08-21 00:01:59 +0530

    Task 3 commit

--- dblayer/dumpdb.c
+++ dblayer/dumpdb.c
@@ -13,7 +13,31 @@ printRow(void *callbackObj, RecId rid, byte *row, int len) {
     Schema *schema = (Schema *) callbackObj;
     byte *cursor = row;
 
-    UNIMPLEMENTED;
+    for(int i=0; i<schema->numColumns; i++){
+        int type=schema->columns[i]->type;
+        char strBuf[256];
+        switch(type){
+            case VARCHAR: 
+                int offset = DecodeCString(cursor, strBuf, sizeof(strBuf));
+                printf("%s",strBuf);
+                cursor += offset;
+                break;
+            
+            case INT:
+                int intVal = DecodeInt(cursor);
+                printf("%d", intVal);
+                cursor += 4;
+                break;
+            
+            case LONG:
+                long long longVal = DecodeLong(cursor);
+                printf("%lld", longVal);
+                cursor+=8;
+                break;
+        }
+        if(i!=schema->numColumns-1)printf(",");
+        else printf("\n");
+    }
 }
 
 #define DB_NAME "data.db"
@@ -21,7 +45,7 @@ printRow(void *callbackObj, RecId rid, byte *row, int len) {
 	 
 void
 index_scan(Table *tbl, Schema *schema, int indexFD, int op, int value) {
-    UNIMPLEMENTED;
+    //UNIMPLEMENTED;
     /*
     Open index ...
     while (true) {
@@ -31,6 +55,25 @@ index_scan(Table *tbl, Schema *schema, int indexFD, int op, int value) {
     }
     close index ...
     */
+    int scan = AM_OpenIndexScan(indexFD, 'i', 4, op, (char *)&value);
+    if(scan<0){
+        AM_PrintError("AM_OpenIndexScan failed.");
+        exit(1);
+    }
+    int rid;
+    byte record[MAX_PAGE_SIZE];
+    while(true){
+        rid = AM_FindNextEntry(scan);
+        if(rid<0)break;
+        int len = Table_Get(tbl, rid, record, sizeof(record));
+        printRow(schema, rid, record, len);
+    }
+
+    int err = AM_CloseIndexScan(scan);
+    if(err<0){
+        AM_PrintError("AM_CloseIndexScan failed.");
+        exit(1);
+    }
 }
 
 int
@@ -39,10 +82,13 @@ main(int argc, char **argv) {
     Schema *schema = parseSchema(schemaTxt);
     Table *tbl;
 
-    UNIMPLEMENTED;
+    //UNIMPLEMENTED;
+    int err = Table_Open(DB_NAME, schema, false, &tbl);
+    checkerr(err);
     if (argc == 2 && *(argv[1]) == 's') {
-	UNIMPLEMENTED;
+	//UNIMPLEMENTED;
 	// invoke Table_Scan with printRow, which will be invoked for each row in the table.
+        Table_Scan(tbl, schema, printRow);
     } else {
 	// index scan by default
 	int indexFD = PF_OpenFile(INDEX_NAME);
